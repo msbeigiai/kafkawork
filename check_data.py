0d35c65f0e564c1ca08eea9efee02ac0f39beb46
msbeigiai <msbeigi83@gmail.com>
2022-03-08 14:00:50 +0330

    removed un necessary files

--- check_data.py
+++
@@ -1,8 +0,0 @@
-class CheckData:
-    def __init__(self):
-        pass
-
-    # if value is in redis fetch it from redis and add it to the existing record and send it via kafka
-
-    def check_if_value_is_exist(self, value):
-        temp_value = r.get(value_key)
